fbeee492a09d1df02a0587b533c3fe8ae8752562
Placide <placidekad@gmail.com>
2025-11-13 04:42:28 +0200

    updated the design of the login page, still need to manage states on upload

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,25 +11,26 @@ const Login=()=>{
       w-full h-full
       flex items-center justify-center
       bg-fuchsia-50">
-      <div className={`w-8/10 max-h-[600px] h-4/10 sm:w-6/10 lg:w-5/10 
+      <div className={`w-8/10 max-h-[300px] max-w-[600px] h-25/100 lg:w-7/10 
       flex flex-col items-center justify-evenly
-      rounded-xl bg-fuchsia-100
-      shadow-md shadow-fuchsia-200 transition-all ease duration-200`}
-      >
+      rounded-xl bg-fuchsia-100 ${windowWidth>1000?'hover:scale-102 shadow-login-hover':'shadow-login'}
+      transition-all ease duration-300`}>
 
         <form 
         id="connect-with-username-password" 
-        className='h-1/2 w-9/10 
+        className='h-3/4 w-9/10 
         flex flex-col justify-evenly items-center relative'>
           <div id='username-zone'
-          className='flex h-4/10 w-full items-center justify-between'>
-            <label htmlFor="username" 
-            className='cursor-pointer w-15/100 h-full text-center grid content-center text-nowrap text-sm'>
-            Nom utilisateur
-            </label>
+          className={`flex h-4/10 w-full items-center ${windowWidth>530?'justify-between':'justify-center'}`}>
+            {windowWidth>530&&
+            <label htmlFor="email" 
+            className='cursor-pointer w-15/100 h-full text-start grid content-center text-nowrap text-sm'>
+            E-mail
+            </label>}
             <input 
-            id='username' 
-            type="text" 
+            placeholder={`${windowWidth<=530?'E-mail':''}`}
+            id='email' 
+            type="email" 
             className='w-75/100 bg-white text-black h-3/5
             pl-2 py-1 rounded-lg outline-none
             border-black border-2
@@ -42,12 +43,14 @@ const Login=()=>{
           </div>
 
           <div id='password-zone'
-          className='flex h-4/10 w-full items-center justify-between'>
+          className={`flex h-4/10 w-full items-center ${windowWidth>530?'justify-between':'justify-center'}`}>
+            {windowWidth>530&&
             <label htmlFor="password" 
-            className='cursor-pointer w-15/100 h-full text-center grid content-center text-nowrap text-sm'>
+            className='cursor-pointer w-15/100 h-full text-start grid content-center text-nowrap text-sm'>
             Mot de passe
-            </label>
+            </label>}
             <input 
+            placeholder={`${windowWidth<=530?'Mot de passe':''}`}
             id='password' 
             type="password" 
             className='w-75/100 bg-white text-black h-3/5
@@ -85,11 +88,10 @@ const Login=()=>{
             </div>:null
           }  
 
-
           <ButtonClikable 
             type='submit'
             p_style="rounded-md py-2 px-8"
-            content='Submit'/>
+            content='Se Connecter'/>
         </form>
       </div>
       <div className="absolute top-0 left-0">
